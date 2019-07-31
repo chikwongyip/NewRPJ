93811d38674954ab96cfc233c2b9de98fe6f0653
chikwongyip <527439024@qq.com>
2019-07-31 21:51:23 +0800

    implement login

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -3,6 +3,22 @@ namespace Admin\Controller;
 use Think\Controller;
 class LoginController extends Controller {
     public function login(){
-        $this->display();
+      if(IS_POST){
+        $userModel = M('Rpj_user');
+        $username = I('post.username');
+        $pwd = I('post.password');
+        $userInfo = $userModel->where(array('username'=>$username))->find();
+        if(!$userInfo){
+          $this->error('用户名错误','',1);
+        }
+        if($userInfo['password'] !== $pwd){
+          $this->error('密码错误','',1);
+        }else{
+          cookie('username',$userInfo['username']);
+          $this->success('登陆成功','/NewRPJ/index.php/Admin/Index/index',1);
+        }
+
+      }
+      $this->display();
     }
 }
--- Application/Runtime/Cache/Admin/0dfec61edd66f450033aa87c28a760f4.php
+++ Application/Runtime/Cache/Admin/0dfec61edd66f450033aa87c28a760f4.php
@@ -3,22 +3,22 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>RPJ-后台系统</title>
-<link href="/chikwong/NewRPJ/Public/style/authority/login_css.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="/chikwong/NewRPJ/Public/scripts/jquery/jquery-1.7.1.js"></script>
+<link href="/NewRPJ/Public/style/authority/login_css.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="/NewRPJ/Public/scripts/jquery/jquery-1.7.1.js"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
 		$("#login_sub").click(function(){
-			$("#submitForm").attr("action", "index.html").submit();
+			$("#submitForm").attr("action", "<?php echo U('admin/login/login');?>").submit();
 		});
 	});
-	
+
 	/*回车事件*/
-	function EnterPress(e){ //传入 event 
-		var e = e || window.event; 
-		if(e.keyCode == 13){ 
-			$("#submitForm").attr("action", "index.html").submit();
-		} 
-	} 
+	function EnterPress(e){ //传入 event
+		var e = e || window.event;
+		if(e.keyCode == 13){
+			$("#submitForm").attr("action", "<?php echo U('admin/login/login');?>").submit();
+		}
+	}
 </script>
 </head>
 <body>
@@ -26,18 +26,18 @@
 		<div id="login_area">
 			<div id="login_box">
 				<div id="login_form">
-					<form id="submitForm" action="" method="post">
+					<form id="submitForm" action="<?php echo U('admin/login/login');?>" method="post">
 						<div id="login_tip">
 							<span id="login_err" class="sty_txt2"></span>
 						</div>
 						<div>
-							 用户名：<input type="text" name="userEntity.userCode" class="username" id="name">
+							 用户名：<input type="text" name="username" class="username" id="name">
 						</div>
 						<div>
-							密&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="userEntity.password" class="pwd" id="pwd" onkeypress="EnterPress(event)" onkeydown="EnterPress()">
+							密&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="password" class="pwd" id="pwd" onkeypress="EnterPress(event)" onkeydown="EnterPress()">
 						</div>
 						<div id="btn_area">
-							<input type="button" class="login_btn" id="login_sub"  value="登  录">
+							<input type="submit" class="login_btn" id="login_sub"  value="登  录">
 							<input type="reset" class="login_btn" id="login_ret" value="重 置">
 						</div>
 					</form>
