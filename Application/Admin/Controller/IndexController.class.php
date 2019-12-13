f8926d12d10aed0337acd06f8f606348dbf65a2e
chikwongyip <527439024@qq.com>
2019-12-13 15:01:09 +0800

    2019-12-13

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
         $url = U('Admin/Introduce/introduce');
-        echo(var_dump($url)) ;
+        //echo(var_dump($url)) ;
         $this->assign('url',$url);
         $this->display();
     }
