8931feb9a5b67a049f2f2d4b7431cb603134eb27
chikwongyip <527439024@qq.com>
2020-03-03 16:42:58 +0800

    mutipule sql

--- Application/Home/Controller/AboutController.class.php
+++ Application/Home/Controller/AboutController.class.php
@@ -8,6 +8,16 @@ class AboutController extends Controller {
         $modelCategory = M('Rpj_procategory');
         $modelProduct = M('Rpj_product');
         $modelBrand = M('Rpj_brand');
+        $modelTest = new \Think\Model();
+        $test=$modelTest->query("select rpj_product.brand_id,
+                                        rpj_product.category_id,
+                                        rpj_brand.brand_name,
+                                        rpj_procategory.category_name
+                                    from rpj_product
+                                    inner join rpj_brand on rpj_product.brand_id = rpj_brand.brand_id
+                                    inner join rpj_procategory on rpj_product.category_id = rpj_procategory.category_id
+                                    group by rpj_product.brand_id,rpj_product.category_id");
+        var_dump($test); exit;
     //  select data
         $company = $model->find(1);
         $category = $modelCategory->select();
