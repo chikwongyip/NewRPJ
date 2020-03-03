993f0a30b52c25fb532a8aea1285e571b30ba607
chikwongyip <527439024@qq.com>
2020-03-03 21:37:46 +0800

    set if tag

--- Application/Home/Controller/AboutController.class.php
+++ Application/Home/Controller/AboutController.class.php
@@ -5,11 +5,17 @@ class AboutController extends Controller {
     public function about(){
     // model inizitial
         $model = M('Rpj_company');
-        $modelCategory = M('Rpj_procategory');
         $modelProduct = M('Rpj_product');
-        $modelBrand = M('Rpj_brand');
-        $modelTest = new \Think\Model();
-        $test=$modelTest->query("select rpj_product.brand_id,
+        $modelBrand = new \Think\Model();
+        $modelCategory = new \Think\Model();
+        $category=$modelCategory->query("select rpj_product.category_id,
+                                                rpj_procategory.category_name
+                                    from rpj_product
+                                    inner join rpj_procategory on rpj_product.category_id = rpj_procategory.category_id
+                                    group by rpj_product.category_id");
+
+
+        $brand=$modelBrand->query("select rpj_product.brand_id,
                                         rpj_product.category_id,
                                         rpj_brand.brand_name,
                                         rpj_procategory.category_name
@@ -17,19 +23,17 @@ class AboutController extends Controller {
                                     inner join rpj_brand on rpj_product.brand_id = rpj_brand.brand_id
                                     inner join rpj_procategory on rpj_product.category_id = rpj_procategory.category_id
                                     group by rpj_product.brand_id,rpj_product.category_id");
-        var_dump($test); exit;
+        // var_dump($brand);
+        // var_dump($category);
+        // exit;
     //  select data
         $company = $model->find(1);
-        $category = $modelCategory->select();
-        $brand  = $modelProduct->field('brand_id,category_id')->group('category_id')->select();
         $product = $modelProduct->select();
-        $brandName = $modelBrand->select();
     // assign data html
         $this->assign('company',$company);
         $this->assign('category',$category);
         $this->assign('brand',$brand);
         $this->assign('product',$product);
-        $this->assign('brandname',$brandName);
         $this->display();
     }
 }
