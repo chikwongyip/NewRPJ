96397c053e15e189493624f321073f26a37fda72
chikwongyip <527439024@qq.com>
2020-04-20 22:01:58 +0800

    submit for search 2020-4-20

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -22,6 +22,16 @@ class IndexController extends Controller
           $this->assign('brand',$brand);
           $this->assign('product',$product);
           $this->assign('brands',$brands);
-          $this->display();
+          if (IS_POST) {
+
+            if (!is_null($_POST["category_id"])) {
+              $this->success('正在为你查询.....',U('home/prodlist/listwithcategory',array('category_id'=>$_POST["category_id"])));
+            }else {
+              $this->success('正在为你查询.....',U('home/prodlist/prodlist'));
+            }
+          }else {
+            $this->display();
+          }
+
     }
 }
