ad77af7c6fbd8a12d2d1033f6d7e7d31b38a4212
chikwongyip <527439024@qq.com>
2021-06-29 10:25:00 +0800

    2021-6-29
    delete log

--- Application/Runtime/Data/_fields/bdm247650322_db.rpj_attachment.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:2:"id";i:1;s:8:"brand_id";i:2;s:11:"category_id";i:3;s:4:"name";i:4;s:3:"url";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:6:"int(5)";s:8:"brand_id";s:6:"int(5)";s:11:"category_id";s:6:"int(5)";s:4:"name";s:12:"varchar(100)";s:3:"url";s:12:"varchar(200)";}}
